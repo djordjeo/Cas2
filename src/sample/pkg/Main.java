067bd3d0822db9e83850694d35377dc129c20a72
Djordje <djordje.ostojic99@gmail.com>
2021-11-17 16:03:19 +0100

    push

--- src/sample/pkg/Main.java
+++ src/sample/pkg/Main.java
@@ -1,4 +1,31 @@
 package sample.pkg;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack);
+        stack.push(10);
+        stack.push(20);
+        System.out.println(stack);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.push(60);
+        System.out.println(stack);
+    }
+
 }
--- src/sample/pkg/Stack.java
+++ src/sample/pkg/Stack.java
@@ -1,4 +1,72 @@
 package sample.pkg;
 
+import java.util.Arrays;
+
 public class Stack {
+
+    private int[] elements;
+    private int head;
+
+    public Stack(){
+        elements = new int[5];
+        head = 0;
+    }
+
+    public Stack(int n){
+        elements = new int[n];
+        head =0;
+    }
+
+    public int[] getElements() {
+        return elements;
+    }
+
+    public int getHead() {
+        return head;
+    }
+
+    public void push(int v){
+        checkCapacity();
+        elements[head++] = v;
+    }
+
+    private int size(){
+        return head;
+    }
+
+    private int capacity(){
+        return elements.length;
+    }
+
+    private void checkCapacity(){
+        if(size() == capacity()){
+            System.out.println("Stack overflow, resizing");
+            elements = Arrays.copyOf(elements,2*elements.length);
+        }
+    }
+
+    public int pop(){
+        if(isEmpty()) {
+            return 0;
+        }
+        head--;
+        int tmp = elements[head];
+        elements[head] = 0;
+        return tmp;
+    }
+
+    public boolean isEmpty(){
+        if(size() == 0){
+            System.out.println("Stack underflow");
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Stack{" +
+                "elements=" + Arrays.toString(elements) + "}" + "\n" +
+                "size: " + size() + " capacity: " + capacity();
+    }
 }
